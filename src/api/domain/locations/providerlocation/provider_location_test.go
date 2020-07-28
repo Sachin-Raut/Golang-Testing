0dcbb87e4a1158f1c00662a099044b71c7aed7da
Sachin Raut <sachinraut@Sachins-Mac-mini.local>
2020-07-28 22:42:38 +0530

    NoInternet

--- src/api/domain/locations/providerlocation/provider_location_test.go
+++ src/api/domain/locations/providerlocation/provider_location_test.go
@@ -1,1 +1,25 @@
 package providerlocation
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetCountryRestClientErrorNoInternet(t *testing.T) {
+	// rest.StartMockupServer()
+	// rest.FlushMockups()
+	// rest.AddMockups(&rest.Mock{
+	// 	URL:          "https://api.mercadolibre.com/countries/AR",
+	// 	HTTPMethod:   http.MethodGet,
+	// 	RespHTTPCode: 0,
+	// })
+
+	country, err := GetCountry("AR")
+
+	assert.Nil(t, country)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusInternalServerError, err.Status)
+	assert.EqualValues(t, "", err.Message)
+}
