9202e550346491a39c2f135fd1a913f6f63dfe31
Sachin Raut <sachinraut@Sachins-Mac-mini.local>
2020-07-28 23:02:07 +0530

    test

--- src/api/domain/locations/providerlocation/provider_location_test.go
+++ src/api/domain/locations/providerlocation/provider_location_test.go
@@ -7,6 +7,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+/*
+run all the tests after disconnecting & reconnecting the internet
+*/
+
 func TestGetCountryRestClientErrorNoInternet(t *testing.T) {
 	// rest.StartMockupServer()
 	// rest.FlushMockups()
@@ -21,5 +25,14 @@ func TestGetCountryRestClientErrorNoInternet(t *testing.T) {
 	assert.Nil(t, country)
 	assert.NotNil(t, err)
 	assert.EqualValues(t, http.StatusInternalServerError, err.Status)
-	assert.EqualValues(t, "", err.Message)
+	assert.EqualValues(t, "invalid rest client response when trying to get country AR", err.Message)
+}
+
+func TestGetCountryNotFound(t *testing.T) {
+	country, err := GetCountry("AR")
+
+	assert.Nil(t, country)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusNotFound, err.Status)
+	assert.EqualValues(t, "Country not found", err.Message)
 }
