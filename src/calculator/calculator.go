902a1a7185d423f993c0e6d2825195b3305c151d
Sachin Raut <sachinraut@Sachins-Mac-mini.local>
2020-08-04 13:28:11 +0530

    calculator.go

---
+++ src/calculator/calculator.go
@@ -0,0 +1,23 @@
+package calculator
+
+//DiscountCalculator is
+type DiscountCalculator struct {
+	minimumPurchaseAmount int
+	discountAmount        int
+}
+
+//NewDiscountCalculator is
+func NewDiscountCalculator(minimumPurchaseamount int, discountAmount int) *DiscountCalculator {
+	return &DiscountCalculator{
+		minimumPurchaseAmount: minimumPurchaseamount,
+		discountAmount:        discountAmount,
+	}
+}
+
+//Calclulate is
+func (c *DiscountCalculator) Calclulate(purchaseAmount int) int {
+	if purchaseAmount > c.minimumPurchaseAmount {
+		return purchaseAmount - c.discountAmount
+	}
+	return purchaseAmount
+}
