ed9a8802969e841f51833a7c070140fac2315db0
Sachin Raut <sachinraut@Sachins-Mac-mini.local>
2020-08-05 10:08:20 +0530

    calculator_test.go

---
+++ src/copycalculator/calculate_test.go
@@ -0,0 +1,20 @@
+package copycalculator
+
+import "testing"
+
+func TestDiscountApplied(t *testing.T) {
+	calculator := NewDiscountCalculator(100, 20)
+	amount := calculator.Calclulate(150)
+	if amount != 130 {
+		t.Error("received-", amount)
+		t.Fail()
+	}
+}
+
+func TestDiscountNotApplied(t *testing.T) {
+	calculator := NewDiscountCalculator(100, 20)
+	amount := calculator.Calclulate(50)
+	if amount != 50 {
+		t.Fail()
+	}
+}
