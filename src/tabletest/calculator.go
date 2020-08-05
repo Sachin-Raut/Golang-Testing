44f70f197ba9c102bae234f358b6971f76fe1ec3
Sachin Raut <sachinraut@Sachins-Mac-mini.local>
2020-08-05 12:21:40 +0530

    calculator.go

--- src/tabletest/calculator.go
+++ src/tabletest/calculator.go
@@ -18,7 +18,7 @@ func NewDiscountCalculator(minimumPurchaseAmount int, discountAmount int) *Disco
 func (c DiscountCalculator) Calculator(purchaseAmount int) int {
 
 	if purchaseAmount > c.minimumPurchaseAmount {
-		multiplier := purchaseAmount - c.minimumPurchaseAmount
+		multiplier := purchaseAmount / c.minimumPurchaseAmount
 		return purchaseAmount - (c.discountAmount * multiplier)
 	}
 	return purchaseAmount
