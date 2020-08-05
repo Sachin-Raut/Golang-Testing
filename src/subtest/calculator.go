d2f85070ea17b117e69d6c610a79907718d34dec
Sachin Raut <sachinraut@Sachins-Mac-mini.local>
2020-08-05 17:44:51 +0530

    calculator.go

---
+++ src/subtest/calculator.go
@@ -0,0 +1,25 @@
+package subtest
+
+//DiscountCalculator is
+type DiscountCalculator struct {
+	minimumPurchaseAmount int
+	discountAmount        int
+}
+
+//NewDiscountCalculator is
+func NewDiscountCalculator(minimumPurchaseAmount int, discountAmount int) *DiscountCalculator {
+	return &DiscountCalculator{
+		minimumPurchaseAmount: minimumPurchaseAmount,
+		discountAmount:        discountAmount,
+	}
+}
+
+//Calculator is
+func (c DiscountCalculator) Calculator(purchaseAmount int) int {
+
+	if purchaseAmount > c.minimumPurchaseAmount {
+		multiplier := purchaseAmount / c.minimumPurchaseAmount
+		return purchaseAmount - (c.discountAmount * multiplier)
+	}
+	return purchaseAmount
+}
