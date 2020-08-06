50e0746bb7e2bc19365b57cb6e95f73926d64de3
Sachin Raut <sachinraut@Sachins-Mac-mini.local>
2020-08-06 21:31:49 +0530

    calculator_test.go

--- src/databasemock/calculator/calculator_test.go
+++ src/databasemock/calculator/calculator_test.go
@@ -1,1 +1,39 @@
 package calculator
+
+import "testing"
+
+type DiscountRepositoryMock struct{}
+
+func (drm DiscountRepositoryMock) FindCurrentDiscount() int {
+	return 20
+}
+
+func TestNewDiscountCalculator(t *testing.T) {
+	type testCase struct {
+		name                  string
+		minimumPurchaseAmount int
+		purchaseAmount        int
+		expectedAmount        int
+	}
+
+	testCases := []testCase{
+		{name: "apply 20", minimumPurchaseAmount: 100, purchaseAmount: 150, expectedAmount: 130},
+		{name: "apply 40", minimumPurchaseAmount: 100, purchaseAmount: 200, expectedAmount: 160},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			discountRepositoryMock := DiscountRepositoryMock{}
+			calculator, err := NewDiscountCalculator(tc.minimumPurchaseAmount, discountRepositoryMock)
+
+			if err != nil {
+				t.Fatalf("could not instantiate the calculator - %s", err.Error())
+			}
+			amount := calculator.Calculate(tc.purchaseAmount)
+
+			if amount != tc.expectedAmount {
+				t.Errorf("expected %v, got %v", tc.expectedAmount, amount)
+			}
+		})
+	}
+}
